56b0038637d6ca804a78292ed27a95c8ad099266
André Alves <andrealves-tec@hotmail.com>
2023-11-01 11:08:37 -0300

    Criação do construtor Cliente

--- src/main/java/banco/app/App.java
+++ src/main/java/banco/app/App.java
@@ -8,9 +8,11 @@ import banco.classes.ContaPoupanca;
 public class App {
 
     public static void main(String[] args) {
-        Cliente cli = new Cliente();
-        cli.setNome("André Alves");
-        cli.setCpf("023.756.888-90");
+    	
+    	String nome = "André Alves";
+    	String cpf = "023.756.888-90";
+    	
+        Cliente cli = new Cliente(nome, cpf);
 
         Conta cc = new ContaCorrente(cli);
         Conta cp = new ContaPoupanca(cli);
--- src/main/java/banco/classes/Cliente.java
+++ src/main/java/banco/classes/Cliente.java
@@ -2,23 +2,29 @@ package banco.classes;
 
 public class Cliente {
 
-    private String nome;
-    private String cpf;
+	private String nome;
+	private String cpf;
 
-    public String getNome() {
-        return nome;
-    }
+	public Cliente(String nome, String cpf) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getCpf() {
-        return cpf;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 
 }
